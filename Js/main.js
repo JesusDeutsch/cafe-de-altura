5a1903ce9c1604e0976acd73e21ee67c05223d3b
Jesus Aleman <167805629+JesusDeutsch@users.noreply.github.com>
2024-07-01 20:45:16 +0200

    update

---
+++ Js/main.js
@@ -0,0 +1,12 @@
+import { dataCoffees } from "/data/dataCoffe.js";
+// // console.log(dataCoffees);
+
+// const container = document.getElementById("prueba")
+
+// const selectImg = dataCoffees[4];
+
+// const imgElemnt= document.createElement("img");
+
+// imgElemnt.src = selectImg.img;
+
+// container.appendChild(imgElemnt);
---
+++ data/dataCoffe.js
@@ -0,0 +1,51 @@
+export const dataCoffees = [
+    {
+        id: "1",
+        nameCoffe: "Costa Rica Tarrazú",
+        price: 9.00,
+        img: "/assets/coffees/coffe_costa_rica_tarrazu.png"
+    },
+    {
+        id: "2",
+        nameCoffe: "Colombia Los Naranjos",
+        price: 9.00,
+        img: "/assets/coffees/coffe_colombia_los_naranjos.png"
+    },
+    {
+        id: "3",
+        nameCoffe: "Laos Amanecer",
+        price: 9.00,
+        img: "/assets/coffees/coffe_laos_amanecer.png"
+    },
+    {
+        id: "4",
+        nameCoffe: "Etiopía Yrgacheff",
+        price: 9.00,
+        img: "/assets/coffees/coffe_etiopia_yrgacheff.png"
+    },
+    {
+        id: "5",
+        nameCoffe: "Kenia Ndunduri ",
+        price: 15.00,
+        img: "/assets/coffees/coffe_kenia_ndunduri.png"
+    },
+    {
+        id: "6",
+        nameCoffe: "Etiopía Sidamo",
+        price: 17.00,
+        img: "/assets/coffees/coffe_etiopia_sidamo.png"
+    },
+    {
+        id: "7",
+        nameCoffe: "Costa Rica Monte Bello",
+        price: 12.00,
+        img: "/assets/coffees/coffe_costa_rica_monte_bello.png"
+    },
+    {
+        id: "8",
+        nameCoffe: "Colombia La Casita",
+        price: 9.00,
+        img: "/assets/coffees/coffe_colombia_la_casita.png",
+        available: false
+    }
+    ]
